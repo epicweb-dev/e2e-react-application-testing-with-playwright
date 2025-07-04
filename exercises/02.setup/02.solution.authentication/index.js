48eccd7bb3e711e4f7c55d7e9836998e6c60ea2f
Artem Zakharchenko <kettanaito@gmail.com>
2025-07-04 11:11:01 +0200

    02/02: remove unused e2e setup

--- exercises/02.setup/02.solution.authentication/index.js
+++ exercises/02.setup/02.solution.authentication/index.js
@@ -16,10 +16,6 @@ sourceMapSupport.install({
 	},
 })
 
-if (process.env.MOCKS === 'true') {
-	await import('./tests/mocks/index.ts')
-}
-
 if (process.env.NODE_ENV === 'production') {
 	await import('./server-build/index.js')
 } else {
--- exercises/02.setup/02.solution.authentication/tests/setup/setup-test-env.ts
+++ exercises/02.setup/02.solution.authentication/tests/setup/setup-test-env.ts
@@ -5,10 +5,8 @@ import '#app/utils/env.server.ts'
 
 import { cleanup } from '@testing-library/react'
 import { afterEach, beforeEach, vi, type MockInstance } from 'vitest'
-import { server } from '#tests/mocks/index.ts'
 import './custom-matchers.ts'
 
-afterEach(() => server.resetHandlers())
 afterEach(() => cleanup())
 
 export let consoleError: MockInstance<(typeof console)['error']>
