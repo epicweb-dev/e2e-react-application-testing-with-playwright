8a19438018e05c59c8b9d47f38433cac8eaaf441
Artem Zakharchenko <kettanaito@gmail.com>
2025-07-22 19:26:51 +0200

    03/01: remove test actions

--- exercises/03.guides/01.problem.recording-interactions/tests/e2e/profile-edit.test.ts
+++ exercises/03.guides/01.problem.recording-interactions/tests/e2e/profile-edit.test.ts
@@ -7,16 +7,4 @@ test('saves changes to the name of the user', async ({
 }) => {
 	await authenticate({ as: 'user' })
 	await navigate('/settings/profile')
-
-	await page.getByRole('textbox', { name: 'Name', exact: true }).click()
-	await page
-		.getByRole('textbox', { name: 'Name', exact: true })
-		.press('ControlOrMeta+a')
-	await page
-		.getByRole('textbox', { name: 'Name', exact: true })
-		.fill('John Doe')
-	await page.getByRole('button', { name: 'Save changes' }).click()
-	await page.getByRole('link', { name: 'John Doe John Doe' }).click()
-	await page.getByRole('menuitem', { name: 'Profile' }).click()
-	await expect(page.getByRole('heading', { name: 'John Doe' })).toBeVisible()
 })
