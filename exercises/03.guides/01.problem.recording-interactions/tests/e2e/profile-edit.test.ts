79f5f928eaf5ca90dd2ab84fc4f5f231cece388e
Artem Zakharchenko <kettanaito@gmail.com>
2025-08-05 10:43:27 +0200

    03/01: add steps to the test file

--- exercises/03.guides/01.problem.recording-interactions/tests/e2e/profile-edit.test.ts
+++ exercises/03.guides/01.problem.recording-interactions/tests/e2e/profile-edit.test.ts
@@ -7,4 +7,9 @@ test('saves changes to the name of the user', async ({
 }) => {
 	await authenticate({ as: 'user' })
 	await navigate('/settings/profile')
+
+	// 🐨 In the Playwright extension for Visual Studio Code,
+	// enable the "Show browser" option and run this test.
+	// Then, place your cursor after this comment block and click
+	// "Record at cursor" from the Playwright extension panel.
 })
