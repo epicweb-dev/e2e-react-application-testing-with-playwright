1819a7f284fe7b259d1d48c3a109b8601845f7a1
Artem Zakharchenko <kettanaito@gmail.com>
2025-11-04 11:17:19 +0100

    add invalid credentials basic auth test case

--- exercises/02.authentication/01.solution.basic/tests/e2e/authentication.test.ts
+++ exercises/02.authentication/01.solution.basic/tests/e2e/authentication.test.ts
@@ -12,3 +12,18 @@ test('authenticates using a email and password', async ({ navigate, page }) => {
 
 	await expect(page.getByText(user.name!)).toBeVisible()
 })
+
+test('displays an error message when authenticating with invalid credentials', async ({
+	navigate,
+	page,
+}) => {
+	await navigate('/login')
+
+	await page.getByLabel('Username').fill('non_existing_user')
+	await page.getByLabel('Password').fill('non_existing_password')
+	await page.getByRole('button', { name: 'Log in' }).click()
+
+	await expect(
+		page.getByRole('alert').getByText('Invalid username or password'),
+	).toBeVisible()
+})
