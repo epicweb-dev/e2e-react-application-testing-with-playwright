6183cc8d92b4e7a286a0f7a0ace03f1e9595c190
Artem Zakharchenko <kettanaito@gmail.com>
2025-11-04 11:07:37 +0100

    02/01: add basic auth test case

---
+++ exercises/02.authentication/01.solution.basic/tests/e2e/authentication.test.ts
@@ -0,0 +1,14 @@
+import { createUser } from '#tests/db-utils.ts'
+import { test, expect } from '#tests/test-extend.ts'
+
+test('authenticates using a email and password', async ({ navigate, page }) => {
+	await using user = await createUser()
+
+	await navigate('/login')
+
+	await page.getByLabel('Username').fill(user.username)
+	await page.getByLabel('Password').fill(user.password)
+	await page.getByRole('button', { name: 'Log in' }).click()
+
+	await expect(page.getByText(user.name!)).toBeVisible()
+})
