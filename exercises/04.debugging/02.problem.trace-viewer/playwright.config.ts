e85d51c8767616ecb61af483de277720f056cd05
Artem Zakharchenko <kettanaito@gmail.com>
2026-02-24 15:51:49 +0100

    use .env.test consistently in exercises

--- exercises/04.debugging/02.problem.trace-viewer/playwright.config.ts
+++ exercises/04.debugging/02.problem.trace-viewer/playwright.config.ts
@@ -1,5 +1,12 @@
 import { defineConfig, devices } from '@playwright/test'
-import 'dotenv/config'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+dotenv.config({
+	path: new URL('./.env.test', import.meta.url),
+	override: true,
+})
 
 const PORT = process.env.PORT || '3000'
 
