1b2e273e9f704ac5991f22732524fcdc904ce36e
Artem Zakharchenko <kettanaito@gmail.com>
2026-05-04 12:21:51 +0200

    03/03: add exercise texts

--- exercises/03.guides/03.solution.recording-tests/tests/e2e/profile-edit.test.ts
+++ exercises/03.guides/03.solution.recording-tests/tests/e2e/profile-edit.test.ts
@@ -8,10 +8,17 @@ test('saves changes to the name of the user', async ({
 	await authenticate({ as: 'user' })
 	await navigate('/settings/profile')
 
-	await page.getByRole('textbox', { name: 'Username' }).click()
-	await page.getByRole('textbox', { name: 'Username' }).press('ControlOrMeta+a')
-	await page.getByRole('textbox', { name: 'Username' }).fill('new_username')
+	await page.getByRole('textbox', { name: 'Name', exact: true }).click()
+	await page
+		.getByRole('textbox', { name: 'Name', exact: true })
+		.press('ControlOrMeta+a')
+	await page
+		.getByRole('textbox', { name: 'Name', exact: true })
+		.fill('John Doe')
 	await page.getByRole('button', { name: 'Save changes' }).click()
 
-	await expect(page.getByRole('textbox', { name: 'Username' })).toBeVisible()
+	await page.getByRole('link', { name: 'John Doe' }).click()
+	await page.getByRole('menuitem', { name: 'Profile' }).click()
+
+	await expect(page.getByRole('heading', { name: 'John Doe' })).toBeVisible()
 })
